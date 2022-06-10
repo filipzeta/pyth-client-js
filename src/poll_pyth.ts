b358a72948be5a3ec9b85af5b6b2a6a3bbb7b5b1
Filip <filip@sierra.team>
2022-06-10 15:45:23 +1000

    add poll_pyth

---
+++ src/poll_pyth.ts
@@ -0,0 +1,39 @@
+import { Cluster, clusterApiUrl, Connection } from '@solana/web3.js'
+import { PythConnection } from './PythConnection'
+import { getPythProgramKeyForCluster } from './cluster'
+import { PriceStatus } from '.'
+
+var fs = require('fs')
+const SOLANA_CLUSTER_NAME: Cluster = 'mainnet-beta'
+const connection = new Connection(clusterApiUrl(SOLANA_CLUSTER_NAME))
+const pythPublicKey = getPythProgramKeyForCluster(SOLANA_CLUSTER_NAME)
+
+const pythConnection = new PythConnection(connection, pythPublicKey)
+
+pythConnection.onPriceChange(async (product, price) => {
+  if (product.symbol == 'Crypto.SOL/USD') {
+    // sample output:
+    // SRM/USD: $8.68725 ±$0.013
+
+    let slot = await connection.getSlot()
+
+    if (
+      slot - price.aggregate.publishSlot > 25 ||
+      price.numQuoters < price.minPublishers ||
+      price.status !== PriceStatus.Trading
+    ) {
+      let content = `serverTime=${new Date(Date.now()).toUTCString()}, price=${price.price?.toFixed(2)}, status=${
+        PriceStatus[price.status]
+      }, publishers=${price.numQuoters}>${price.minPublishers}, lastSlot=${price.lastSlot}, validSlot=${
+        price.validSlot
+      } publishSlot=${price.aggregate.publishSlot}, connectionSlot=${slot}`
+
+      fs.writeFile('bad-prices.log', content + '\n', { flag: 'a+' }, (_err: any) => {})
+      // console.log(content)
+    }
+  }
+})
+
+// tslint:disable-next-line:no-console
+// console.log('Reading from Pyth price feed...')
+pythConnection.start()
